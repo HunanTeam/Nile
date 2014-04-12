f5fde30f29e445e0ac55a0fde57c5d5bf43cf54c
somtomking <liust@foxmail.com>
2014-04-12 17:06:42 +0800

    项目提交

--- Nile.Web/App_Start/RouteConfig.cs
+++ Nile.Web/App_Start/RouteConfig.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Nile.Core.Infrastructure;
+using Nile.Web.Framework.Mvc.Routes;
 
 namespace Nile.Web
 {
@@ -11,13 +13,19 @@ namespace Nile.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("favicon.ico");
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //register custom routes (plugins, etc)
+            var routePublisher = EngineContext.Current.Resolve<IRoutePublisher>();
+            routePublisher.RegisterRoutes(routes);
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                "Default", // Route name
                "{controller}/{action}/{id}", // URL with parameters
                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-               new[] { "Nop.Web.Controllers" }
+               new[] { "Nile.Web.Controllers" }
             );
         }
     }
